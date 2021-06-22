a1921bcfbcbf6470eb3392782e459e972fbe3e3b
anubhav-beep <70649851+anubhav-beep@users.noreply.github.com>
2021-06-22 22:12:16 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,33 +8,24 @@ terraform {
 }
 
 provider "azurerm" {
-  # The "feature" block is required for AzureRM provider 2.x.
-  # If you're using version 1.x, the "features" block is not allowed.
-  version = "~>2.0"
   features {}
 }
 
-data "azurerm_client_config" "current" {}
-
-#Create Resource Group
-resource "azurerm_resource_group" "tamops" {
+resource "azurerm_resource_group" "example" {
   name     = "tamopstfstates"
-  location = "eastus2"
+  location = "East US 2"
 }
 
-#Create Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "tamops-vnet"
-  address_space       = ["192.168.0.0/16"]
-  location            = "eastus2"
-  resource_group_name = azurerm_resource_group.tamops.name
-}
+module "vnet" {
+  source              = "Azure/vnet/azurerm"
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
-# Create Subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.tamops.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "192.168.0.0/24"
+  subnet_service_endpoints = {
+    subnet2 = ["Microsoft.Storage", "Microsoft.Sql"],
+    subnet3 = ["Microsoft.AzureActiveDirectory"]
+  }
+  depends_on = [azurerm_resource_group.example]
 }
-  
