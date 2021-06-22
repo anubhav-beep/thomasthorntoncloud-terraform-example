0c6aea942757611690c63849a2fc41c33a1fcea6
anubhav-beep <70649851+anubhav-beep@users.noreply.github.com>
2021-06-22 22:18:40 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tamopstfstates"
-    storage_account_name = "tamopstf43323"
-    container_name       = "tfstatedevops"
-    key                  = "terraformgithubexample.tfstate"
-  }
-}
-
 provider "azurerm" {
   features {}
 }
